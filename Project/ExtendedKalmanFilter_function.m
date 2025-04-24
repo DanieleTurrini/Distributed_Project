b84890398b6f8d50edb482cb84b88899b2cd1524
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 <giacomo.bianchi-2@studenti.unitn.it>
2025-04-24 12:17:05 +0200

    Probabilita delle misure

--- Project/ExtendedKalmanFilter_function.m
+++ Project/ExtendedKalmanFilter_function.m
@@ -1,26 +1,32 @@
 function [x, P] = ExtendedKalmanFilter_function(states_est, measure, control, A, G, fun, Q, H, R, P, count, meas_freq_GPS, meas_freq_ultr, meas_freq_gyr, dt)
     
-    % Prediction step
+    % Measure Probability
+    probGPS = 0.9;
+    probUltr = 0.8;
+    probGyr = 0.7;
+
+    %% Prediction step
     x = fun(states_est, control, dt);      % State prediction
     A_k = A(control, states_est(4), dt);    
     G_k = G(states_est(4), dt);
     P = A_k * P * A_k' + G_k * Q * G_k';   % Covariance prediction
-    
-    if mod(count, meas_freq_GPS) == 0
+
+    %% Update step
+    if mod(count, meas_freq_GPS) == 0 && rand(1) <= probGPS
 
         H = H(1:2,:);
         R = R(1:2,1:2);
         measure = measure(1:2);
         [x,P] = update_step(x, measure, H, P, R);
 
-    elseif mod(count, meas_freq_ultr) == 0
+    elseif mod(count, meas_freq_ultr) == 0 && rand(1) <= probUltr
 
         H = H(3,:);
         R = R(3,3);
         measure = measure(3);
         [x,P] = update_step(x, measure, H, P, R);
 
-    elseif mod(count, meas_freq_gyr) == 0
+    elseif mod(count, meas_freq_gyr) == 0 && rand(1) <= probGyr
 
         H = H(4,:);
         R = R(4,4);
