e2bc0ef375440faf9e44607f9bd27d9a61ba350c
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 <giacomo.bianchi-2@studenti.unitn.it>
2025-04-25 11:09:04 +0200

    EKF probabilita

--- Project/ExtendedKalmanFilter_function.m
+++ Project/ExtendedKalmanFilter_function.m
@@ -1,6 +1,6 @@
 function [x, P] = ExtendedKalmanFilter_function(states_est, measure, control, A, G, fun, Q, H, R, P, count, meas_freq_GPS, meas_freq_ultr, meas_freq_gyr, dt)
     
-    % Measure Probability
+    %% Measure Probability
     probGPS = 0.9;
     probUltr = 0.8;
     probGyr = 0.7;
@@ -10,25 +10,22 @@ function [x, P] = ExtendedKalmanFilter_function(states_est, measure, control, A,
     A_k = A(control, states_est(4), dt);    
     G_k = G(states_est(4), dt);
     P = A_k * P * A_k' + G_k * Q * G_k';   % Covariance prediction
-
+    
     %% Update step
+    % GPS
     if mod(count, meas_freq_GPS) == 0 && rand(1) <= probGPS
 
-        H = H(1:2,:);
-        R = R(1:2,1:2);
-        measure = measure(1:2);
-        [x,P] = update_step(x, measure, H, P, R);
+        [x,P] = update_step(x, measure(1:2), H(1:2,:), P, R(1:2,1:2));
+    end
+    % Ultrasonic
+    if mod(count, meas_freq_ultr) == 0 && rand(1) <= probUltr
 
-    elseif mod(count, meas_freq_ultr) == 0 && rand(1) <= probUltr
+        [x,P] = update_step(x, measure(3), H(3,:), P, R(3,3));
+    end
+    % Gyroscope
+    if mod(count, meas_freq_gyr) == 0 && rand(1) <= probGyr
 
         [x,P] = update_step(x, measure(4), H(4,:), P, R(4,4));
-
-    elseif mod(count, meas_freq_gyr) == 0 && rand(1) <= probGyr
-
-        H = H(4,:);
-        R = R(4,4);
-        measure = measure(4);
-        [x,P] = update_step(x, measure, H, P, R);
 
     end
     
